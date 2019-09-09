3cfc1dd4d1a2f54f614e4ddd31be82bcc1656641
xiaohua <jiafeimao@caibaopay.com>
2019-09-09 15:30:07 +0800

    commit

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,10 @@ import {combineReducers} from './combineReducers';
 
 import {bindActionCreators} from './bindActionCreators'
 
-function sum(a,b){
-    return a + b;
-}
-
 console.log(createStore,combineReducers,bindActionCreators,sum)
 
 export {
     createStore,
     combineReducers,
     bindActionCreators,
-    sum
 }
